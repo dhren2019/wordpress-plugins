610b02691952a81cdcf30f216f267ca7fcb56e72
Dhren <51840371+dhren2019@users.noreply.github.com>
2021-09-16 14:43:08 +0200

    Actions Hooks

--- dhren-plugin.php
+++ dhren-plugin.php
@@ -8,3 +8,13 @@
 *Author URI: https://Goraiko.es
 *License: GLP 2+
 */
+
+//Dn son mis iniciales para que no coincida  con otra funcion igual
+//wp_head -> Es donde se situara la frase
+
+add_action('wp_head', 'dn_muestra_frase' );
+
+function dn_muestra_frase() {
+    echo '<h2>Hola Dhren</h2>';
+    
+}
