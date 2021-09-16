6345578a0a77f7dfc5817a1cab8a924775becca9
dhren2019 <roglobar10@gmail.com>
2021-09-17 00:19:54 +0200

    Custom Posts Types

---
+++ custom-post-type-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+*Plugin Name: Mi tercer plugin
+*Plugin URI: https://Goraiko.es
+*Description: Este es un plugin desarrollado por Dhren para Wordpress para funcionamiento custom post types.
+*Version: 0.02
+*Author: Dhren
+*Author URI: https://Goraiko.es
+*License: GLP 2+
+*/
