966e5f16073a401e99852c03d337ebfcf6f5b70f
dhren2019 <roglobar10@gmail.com>
2021-09-16 11:20:20 +0200

    Initial commit

---
+++ dhren-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+*Plugin Name: Mi primer plugin
+*Plugin URI: https://Goraiko.es
+*Description: Este es un plugin desarrollado por Dhren para Wordpress
+*Version: 0.01
+*Author: Dhren
+*Author URI: https://Goraiko.es
+*License: GLP 2+
+*/
