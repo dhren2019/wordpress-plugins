f4f6addc3d8bb7583bcb11a5e151abd51a4d9914
dhren2019 <roglobar10@gmail.com>
2021-09-17 08:38:25 +0200

    Custom post types v2

--- custom-post-type-plugin.php
+++ custom-post-type-plugin.php
@@ -8,3 +8,24 @@
 *Author URI: https://Goraiko.es
 *License: GLP 2+
 */
+
+//* Public true permite ver el post type en el menú
+//* https://codex.wordpress.org/Function_Reference/register_post_type
+
+ function dn_crear_cpt_libro () {
+
+    $opciones  = array (
+        'public'              => true,
+        'label'               => 'Libros',
+        'exclude_from_search' => true,
+        'menu_position'       => 5,
+        'menu_icon'           => 'dashicons_book',
+        //'has_archive'         => true
+
+    );
+
+     register_post_type( 'libro', $opciones );
+ }
+
+//* Init es cuando la acción va a empezar.
+ add_action('init', 'dn_crear_cpt_libro');
